fb03e05dedcf534a11b3ff8d9ce12f30bcc67853
郝一鸣 <56924259+hacker-hym@users.noreply.github.com>
2024-04-29 11:33:22 +0100

    用户收藏的剧本User Favorite Screenplay/Story

--- src/main/java/com/uos/comp6239backend/controller/TUserController.java
+++ src/main/java/com/uos/comp6239backend/controller/TUserController.java
@@ -4,6 +4,8 @@ import com.uos.comp6239backend.service.TUsersService;
 import com.uos.comp6239backend.utils.ResponseMap;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,7 @@ import java.util.Map;
 @RequestMapping("/TUserCtrl")
 @Api(value = "用户管理", tags = "User Management")
 public class TUserController {
+    private final static Logger log = LoggerFactory.getLogger(TUserController.class);
     @Resource
     private TUsersService tUsersService;
 
@@ -35,6 +38,7 @@ public class TUserController {
     @PostMapping("/tUsersInsert")
     @ApiOperation(value = "添加新用户", notes = "创建并返回一个新用户")
     public ResponseMap.ResultData tUsersInsert(@RequestBody Map<String,Object> values, HttpServletRequest request){
+        log.info("添加新用户");
         return tUsersService.tUsersInsert(values);
     }
 }
